3de818e1e9eacd63d0d263d16acf0ace80d76f69
newbiemasih <newbie.masih01@gmail.com>
2013-06-01 05:51:50 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TesApp::Application.config.secret_key_base = '9538bce9746129102a0c16a240dcb0a1f3bd35d8002d5b9ba76ac4ccf97ef962e2f3a15085183846d97d43e655ff383118717abdcd7839d5fc1de3908234fae1'
